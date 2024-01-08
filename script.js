5f858380ad69d324c42f3fadcd0ef2a9a52ee787
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-01-08 13:55:35 +0100

    update styling + js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const url = "https://fakestoreapi.com/products";
 const products = document.getElementById("products");
+const btns = document.querySelectorAll(".btn");
 
 function getData() {
   fetch(url)
@@ -7,27 +8,83 @@ function getData() {
       if (!resp.ok) throw new Error("Gick inte bra");
       return resp.json();
     })
-    .then(editJson)
-
+    .then((data) => {
+      editJson(data);
+      displayButtons(data);
+      console.log(data);
+    })
     .catch(console.warn);
+}
+
+function editJson(data) {
+  products.innerHTML = data
+    .map(({ image, title, description, price, category }) => {
+      return `
+        <div class="card" data-category="${category}">
+        <div class="card-top">
+          <div class="cards">
+            <img class="card-img" src="${image}" alt="" />
+            <h2 class="card-title">${title}</h2>
+            <p>${description}</p>
+            </div>
+            <div class="card-buy">
+             <p> <span>$</span> ${price}</p>
+              <div class="card-add>
+                <p id="minus">- 0 +</p>
+                 
+              </div>
+            
+            </div>
+          </div>
+        </div>`;
+    })
+    .join("");
+}
+
+function displayButtons(data) {
+  const categories = [
+    "Shoppa loss",
+    ...new Set(data.map((item) => item.category)),
+  ];
+  const buttonContainer = document.querySelector(".btn-container");
+
+  buttonContainer.innerHTML = categories
+    .map((category) => {
+      return `<a href="#" class="btn" data-filter="${category}">${category}</a>`;
+    })
+    .join("");
+
+  const btns = document.querySelectorAll(".btn");
+  btns.forEach((btn) => {
+    btn.addEventListener("click", handleFilter);
+  });
+}
+
+function handleFilter(e) {
+  const filter = e.target.dataset.filter;
+  const storeProducts = document.querySelectorAll(".card");
+
+  storeProducts.forEach((product) => {
+    if (filter === "Shoppa loss" || product.dataset.category === filter) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+
+  const current = document.querySelector(".btn.active");
+  current.classList.remove("active");
+  e.target.classList.add("active");
 }
 
 getData();
 
-function editJson(data) {
-  products.innerHTML = data
-    .map(({ image, title, description, price }) => {
-      return `
-    <div class="card">
-    <div class="cards">
-      <img class="card-img" src="${image}" alt="" />
-      <h2 class="card-title">${title}"</h2>
-      <p>${description}</p>
-      <div class="card-buy">
-      <p>${price}<span>Kr</span></p>
-      <button>köp</button>
-      </div>
-    </div></div>`;
-    })
-    .join("");
+{
+  /* <div class="btn-container">
+<a href="#" class="btn active" data-filter="Shoppa loss">Shoppas loss</a>
+<a href="#" class="btn" data-filter="men's clothing"></a>>Mens clothings</button>
+<a href="#" class="btn" data-filter="jewelery">Jewelery</a>
+<a href="#" class="btn" data-filter="electronics">electronics</a>
+<a href="#" class="btn"data-filter="women's clothing">Womens clothing</a>
+</div> */
 }
